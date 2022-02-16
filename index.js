27d405d1d1b5a8effabf6ec676febc32cfb809b0
ChevalierPierre <pierre.chevalier@epitech.eu>
2022-02-17 00:04:26 +0100

    music

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const db = require('./db.json')
 require('dotenv').config()
 const Discord = require('discord.js')
 const { Intents } = require('discord.js')
-const client = new Discord.Client({ partials: ['MESSAGE'],intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+const client = new Discord.Client({ partials: ['MESSAGE'], intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 client.login(process.env.BOT_TOKEN)
 
 client.on("ready", () => {
@@ -10,9 +10,9 @@ client.on("ready", () => {
 })
 client.on("messageCreate", msg => {
   if (msg.content === "dameno") {
-    const keys = Object.keys(db)
+    const keys = Object.keys(db["works"])
     const nb = (Math.floor(Math.random() * keys.length)).toString()
-    msg.reply(`Here is a suggestion : ${db[nb]["title"]}.\n\n${db[nb]["description"]}.\n\n${db[nb]["url"]}`)
+    msg.reply(`Here is a suggestion : ${db["works"][nb]["title"]}.\n\n${db["works"][nb]["description"]}.\n\n${db["works"][nb]["url"]}`)
     msg.react("❤️")
   }
 })
