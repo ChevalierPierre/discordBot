301ad8572e746fb77cfbe1886b76ef35666c9b9c
ChevalierPierre <pierre.chevalier@epitech.eu>
2022-06-12 10:35:59 +0200

    cronjob

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const db = require('./db.json')
+const cron = require('node-cron')
 require('dotenv').config()
 const Discord = require('discord.js')
 const { Intents } = require('discord.js')
 const client = new Discord.Client({ partials: ['MESSAGE'], intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 client.login(process.env.BOT_TOKEN)
 
+cron.schedule("* * * * * *", function() {
+  Client.users.get("329642548304019457").send("...")
+})
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
+
 client.on("messageCreate", msg => {
   if (msg.content === "dameno") {
     const keys = Object.keys(db["works"])
